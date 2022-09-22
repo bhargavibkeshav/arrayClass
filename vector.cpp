c59da3e0da45c7373ca5236596f42668a50d042d
bhargavibkeshav <bhargavi.b.keshav@gmail.com>
2022-09-21 20:38:46 -0500

    commit vector class

---
+++ vector.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+
+using namespace std;
+
+template<typename T>
+class Vector
+{
+    T* m_data = nullptr;
+    size_t m_size = 0;
+    size_t m_cap = 0;
+
+    void Realloc(size_t new_cap)
+    {
+        T* new_block = new T[new_cap];
+        //if shrinking
+        if(m_size > new_cap)
+        {
+            m_size = new_cap;
+        }
+        //if growing
+        for(int i =0;i<m_size;i++)
+        {
+            new_block[i] = m_data[i];
+        }
+        delete[] m_data;
+        m_data = new_block;
+        m_cap = new_cap;
+    }
+ public:
+    Vector()
+    {
+        Realloc(2);
+    }
+    void pushBack(const T& value)
+    {
+        if(m_size >= m_cap)
+        {
+            Realloc(m_cap + (m_cap/2));
+        }
+        m_data[m_size] = value;
+        m_size++;
+    }
+    size_t Size() const
+    {
+        return m_size;
+    }
+    T& operator[](size_t index)
+    {
+        return m_data[index];
+    }
+    const T& operator[](size_t index) const
+    {
+        return m_data[index];
+    }
+
+
+};
+
+int main()
+{
+    Vector<int> v;
+    v.pushBack(5);
+    v.pushBack(2);
+    v.pushBack(1);
+    cout<<v[0]<<" "<<v[1]<<" "<<v[2]<<" "<<v.Size();
+}
